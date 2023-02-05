4ca2bfc8e654e30043e5da56351981b807c88fe4
Valeria Tyan <vlrtyan@yandex.ru>
2023-02-05 17:10:22 +0300

    fix: card price

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,11 +4,29 @@ import "./Card.scss";
 import { removeSymbols } from "../../utils/constants";
 
 function Card(props) {
-  let {id} = useParams();
+  let { id } = useParams();
   let products = props.productsData;
   const product = products.find(
     (product) => String(removeSymbols(product.name)) === id
   );
+
+  const price = (priceType) => {
+    const separator = "," && ".";
+    priceType = String(priceType);
+    return (
+      <>
+        <p className="card__sum_name_rub">{priceType.split(separator)[0]}</p>
+        {priceType.split(separator)[1] && (
+          <p className="card__sum_name_kopeck">
+            {priceType.split(separator)[1].length < 2
+              ? priceType.split(separator)[1] * 10
+              : priceType.split(separator)[1]}
+          </p>
+        )}
+        <p>₽</p>
+      </>
+    );
+  };
 
   return (
     <main className="card">
@@ -25,7 +43,11 @@ function Card(props) {
             <p className="card__discount">{`-${product.discount}%`}</p>
           )}
           {product.logo_url && (
-            <img className="card__logo" src={product.logo_url} alt="Логотип бренда" />
+            <img
+              className="card__logo"
+              src={product.logo_url}
+              alt="Логотип бренда"
+            />
           )}
         </div>
         <div className="card__body">
@@ -37,57 +59,43 @@ function Card(props) {
             />
           )}
           <h1 className="card__name">{product.name}</h1>
-          {product.new_price ? (
-            <>
-              <div className="card__price card__price_type_old">
-                <p className="card__sum card__sum_type_old">{`${product.old_price} ₽`}</p>
-                <p className="card__price-type">Старая цена</p>
+          {product.new_price && (
+            <div className="card__price card__price_type_old">
+              <div className="card__sum card__sum_type_old">
+                {price(product.old_price)}
               </div>
-              <div className="card__price card__price_type_new">
-                <div className="star-wrapper">
-                  <span className="star-wrapper__star">
-                    {product.stars >= 1 ? <>&#9733;</> : <>&#9734;</>}
-                  </span>
-                  <span className="star-wrapper__star">
-                    {product.stars >= 2 ? <>&#9733;</> : <>&#9734;</>}
-                  </span>
-                  <span className="star-wrapper__star">
-                    {product.stars >= 3 ? <>&#9733;</> : <>&#9734;</>}
-                  </span>
-                  <span className="star-wrapper__star">
-                    {product.stars >= 4 ? <>&#9733;</> : <>&#9734;</>}
-                  </span>
-                  <span className="star-wrapper__star">
-                    {product.stars === 5 ? <>&#9733;</> : <>&#9734;</>}
-                  </span>
-                </div>
-                <p className="card__sum card__sum_type_new">{`${product.new_price} ₽`}</p>
-                <p className="card__price-type">Цена по акции</p>
-              </div>
-            </>
-          ) : (
-            <div className="card__price card__price_type_new">
-              <div className="star-wrapper">
-                <span className="star-wrapper__star">
-                  {product.stars >= 1 ? <>&#9733;</> : <>&#9734;</>}
-                </span>
-                <span className="star-wrapper__star">
-                  {product.stars >= 2 ? <>&#9733;</> : <>&#9734;</>}
-                </span>
-                <span className="star-wrapper__star">
-                  {product.stars >= 3 ? <>&#9733;</> : <>&#9734;</>}
-                </span>
-                <span className="star-wrapper__star">
-                  {product.stars >= 4 ? <>&#9733;</> : <>&#9734;</>}
-                </span>
-                <span className="star-wrapper__star">
-                  {product.stars === 5 ? <>&#9733;</> : <>&#9734;</>}
-                </span>
-              </div>
-              <p className="card__sum card__sum_type_new">{`${product.old_price} ₽`}</p>
-              <p className="card__price-type">Цена по акции</p>
+              <p className="card__price-type">Старая цена</p>
             </div>
           )}
+          <div className="card__price card__price_type_new">
+            <div className="star-wrapper">
+              <span className="star-wrapper__star">
+                {product.stars >= 1 ? <>&#9733;</> : <>&#9734;</>}
+              </span>
+              <span className="star-wrapper__star">
+                {product.stars >= 2 ? <>&#9733;</> : <>&#9734;</>}
+              </span>
+              <span className="star-wrapper__star">
+                {product.stars >= 3 ? <>&#9733;</> : <>&#9734;</>}
+              </span>
+              <span className="star-wrapper__star">
+                {product.stars >= 4 ? <>&#9733;</> : <>&#9734;</>}
+              </span>
+              <span className="star-wrapper__star">
+                {product.stars === 5 ? <>&#9733;</> : <>&#9734;</>}
+              </span>
+            </div>
+            {product.new_price ? (
+              <div className="card__sum card__sum_type_new">
+                {price(product.new_price)}
+              </div>
+            ) : (
+              <div className="card__sum card__sum_type_new">
+                {price(product.old_price)}
+              </div>
+            )}
+            <p className="card__price-type">Цена по акции</p>
+          </div>
         </div>
         <p className="card__disclaimer">{product.disclaimer}</p>
       </div>
